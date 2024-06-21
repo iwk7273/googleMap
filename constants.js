86186d95880aea071ba8230e8bf1989225aa234a
Ryutaro Iwakami <38845632+iwk7273@users.noreply.github.com>
2024-06-22 00:21:37 +0900

    Update constants.js
    Add key and sheet ID

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const API_KEY = 'API キーを入力してください';
-const SHEET_ID = 'Google スプレッドシートの Sheet ID を入力してください';
+const API_KEY = 'AIzaSyBgSr0GH7RWsWcu5hR3ehk5P5ssyHzRLI0';
+const SHEET_ID = '1ZBvOjWsrj56JbJlAmI72G1h-w1LeJAzT3dxGya8w9uI';
