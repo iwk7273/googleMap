1a964f580f983e43b53709311508a596442ebbdd
Ryutaro Iwakami <38845632+iwk7273@users.noreply.github.com>
2024-06-22 23:50:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -274,8 +274,8 @@ function showPinInfo(marker, data) {
 
 async function editPin(time, lat, lng, nickname, count, tag) {
     document.getElementById('time').value = time;
-    document.getElementById('nickname').value = "";
-    document.getElementById('count').value = "";
+    //document.getElementById('nickname').value = "";
+    //document.getElementById('count').value = "";
     document.getElementById('tag').value = tag;
 
     const form = document.getElementById('form');
